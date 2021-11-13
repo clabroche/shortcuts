e58167e18f107cc55cd3323b9b22c2bd9b5a1d48
clabroche <corentinlabroche@gmail.com>
2021-11-13 19:17:45 +0100

    feat(brightness): sync brightness of all devices

---
+++ README.md
@@ -0,0 +1,22 @@
+# Shortcuts
+
+Little package to link my system shortcuts with advanced commands made in node.js
+
+## OS
+
+Archlinux, kde Plasma 5
+
+## Requirements 
+
+ - nodejs:  >= 14
+
+## Install
+
+``` npm i -g @iryu54/shortcuts ```
+You can link your system shortcuts to the shortcuts package
+
+## Usage
+
+ - Brightness: Sync brightness from all monitors (linux)
+   - shortcuts brightness increase 
+   - shortcuts brightness decrease 
---
+++ plugins/brightness.js
@@ -0,0 +1,58 @@
+const { execSync } = require('child_process')
+const { notify } = require('../notify')
+const step = 0.1
+
+module.exports = {
+  name: 'Brightness',
+  command: 'brightness',
+  requirements: {
+    platforms: [
+      'linux'
+    ],
+    commands: [
+      'notify-send',
+      'xrandr',
+      'grep',
+    ]
+  },
+  execute(options) {
+    const amount = {
+      increase: step,
+      decrease: step * -1
+    }
+    setBrightnessForDevices(amount[options[0]])
+  },
+}
+
+function getDevices() {
+  return execSync('xrandr | grep " connected" | cut -f1 -d " "')
+    .toString().split('\n')
+    .filter(a => a)
+}
+
+function setBrightnessForDevices(amount) {
+  const devices = getDevices()
+  const [nextLevel] = devices.map(device => setBrightnessForDevice(device, amount))
+  notify(module.exports, `${nextLevel * 100}%`, {timeout: 1})
+}
+
+function setBrightnessForDevice(device, amount) {
+  if (!device) throw Error('Device parameter is required ')
+  const brightness = getBrightness(device)
+  let nextLevel = brightness + amount
+  if (nextLevel > 1) nextLevel = 1
+  if (nextLevel < 0) nextLevel = 0
+  execSync(`xrandr --output ${device} --brightness ${nextLevel}`)
+  return nextLevel
+}
+
+function getBrightness(device) {
+  const brightness = +execSync(`xrandr --verbose --current | grep ^"${device}" -A5 | grep "Brightness"`)
+    .toString()
+    .trim()
+    .split('Brightness:')
+  [1]
+    .trim()
+  if (Number.isNaN(brightness)) throw Error('Can\'t parse brightness')
+  return brightness
+}
